bf5425af0594ea7332dcce7c0f19684c0b32d67d
Tamilala <TinaLinTaiwan2005@gmail.com>
2024-12-29 01:40:18 +0800

    added html files

--- php/user/login.php
+++ php/user/login.php
@@ -1,32 +1,43 @@
-<?php
-$stmt = $conn->stmt_init();
+<html>
 
-// Username & password input. Replace $_POST with $_GET if the form method in html is set to "GET"
-$input_username = $_POST["username"];
-$input_password = $_POST["password"];
+<body>
 
-// Password hashing.
-$hashed_password = hash("sha256", $input_password);
+	<?php
+	$stmt = $conn->stmt_init();
 
-// Query with stmt sql command.
-$sql = "SELECT user.user_password AS pwd FROM user WHERE user.username = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $input_username);
+	// Username & password input. Replace $_POST with $_GET if the form method in html is set to "GET"
+	$input_username = $_POST["username"];
+	$input_password = $_POST["password"];
 
-$stmt->execute();
+	// Password hashing.
+	$hashed_password = hash("sha256", $input_password);
 
-$result = $stmt->get_result();
+	// Query with stmt sql command.
+	$sql = "SELECT user.user_password AS pwd FROM user WHERE user.username = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("s", $input_username);
 
-// If the input username is not found in the database(result size = 0), then aborts.
-if(!$result || $result->num_rows != 1){
-    echo "Account not found.<br>";
-    exit();
-}
+	$stmt->execute();
 
-// Check if the password matches the query result.
-// Can use $login_status for further steps.
-$login_status = ($result->fetch_all(MYSQLI_ASSOC)[0]["pwd"] == $hashed_password);
+	$result = $stmt->get_result();
 
-$result->close();
-exit();
-?>
+	// If the input username is not found in the database(result size = 0), then aborts.
+	if (!$result || $result->num_rows != 1) {
+		echo "Account not found.<br>";
+		header("Location: ./login.html");
+		exit();
+	}
+
+	// Check if the password matches the query result.
+	if ($result->fetch_all(MYSQLI_ASSOC)[0]["pwd"] == $hashed_password) {
+		header("Location: ./analyze.html");
+	}
+
+
+	$result->close();
+	exit();
+	?>
+
+</body>
+
+</html>

213bde3d1da2d57f8af54c8c95cc61f23f3ca08f
Tamilala <TinaLinTaiwan2005@gmail.com>
2024-12-29 05:33:17 +0800

    html update

--- php/user/login.php
+++ php/user/login.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YouTube Trending Analytics</title>
-    <link rel="stylesheet" href="styles.css">
-    <script src="script.js" defer></script>
-</head>
-
-<body>
 <?php
 	session_start(); 
 	include "../database/connect.php";
@@ -52,5 +40,3 @@
 	$result->close();
 	header("Location: $redirect");
 ?>
-
-</body>
